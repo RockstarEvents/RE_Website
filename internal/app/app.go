3569be76e916075c2c60f8d021dbf2a435ff2733
RVKrutykh <RvKrutykh@mai.education>
2024-07-06 21:49:55 +0300

    added postgre for users V0.01

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,16 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"eventPlanner/internal/database"
+
+	"context"
+    "encoding/json"
+    
+    "eventPlanner/internal/models"
+    "github.com/gorilla/mux"
+    "github.com/sirupsen/logrus"
+    "net/http"
 )
 
 func RunApp(port string) {
@@ -15,7 +25,37 @@ func RunApp(port string) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	userRepo := repository.NewUserRepository()
+	
+	dsn := "postgres://postgres:postgres@localhost:5432/postgres"
+
+    conn, err := database.ConnectDB(dsn)
+    if err != nil {
+        logrus.Fatalf("Unable to connect to database: %v\n", err)
+    }
+    defer conn.Close(context.Background())
+
+    userRepo := repository.NewUserRepository(conn)
+
+   router := mux.NewRouter()
+   router.HandleFunc("/users/create", func(w http.ResponseWriter, r *http.Request) {
+        var user models.User
+        err := json.NewDecoder(r.Body).Decode(&user)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+        userID, err := userRepo.CreateUser(user)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(fmt.Sprintf("User created with ID: %d", userID)))
+    }).Methods("POST")
+
+    logrus.Info("Starting server on :8082")
+    
+
 	userService := service.NewUserService(userRepo)
 
 	eventRepo := repository.NewEventRepository()
